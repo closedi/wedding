8848224d325e35c81b87786d23323d134866fcbf
S.Smirnov <s.smirnov@yadro.com>
2026-05-11 13:51:12 +0300

    fix for prod

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const REPO_NAME = 'wedding'
 
 export default defineConfig(({ mode }) => ({
-	base: mode === 'production' ? `/${REPO_NAME}/` : '/',
 	build: {
 		outDir: 'dist',
 		emptyOutDir: true
